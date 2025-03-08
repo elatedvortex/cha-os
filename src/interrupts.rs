a4856ffdcab4979db9cd47baecd7d3181699c568
elatedvortex <vortexelated@gmail.com>
2025-03-08 16:39:26 +0530

    Now the kernal recognizes numbers

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -7,18 +7,20 @@ use spin;
 
 pub const PIC_1_OFFSET:u8=32;
 pub const PIC_2_OFFSET:u8=PIC_1_OFFSET +8;
-pub static PICS: spin::Mutex<ChainedPics> =
-    spin::Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
+pub static PICS: spin::Mutex<ChainedPics> =spin::Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        
         unsafe {
             idt.double_fault.set_handler_fn(double_fault_handler).set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX); 
         }
+        
         idt[InterruptIndex::Timer.as_u8()].set_handler_fn(timer_interrupt_handler);
-   idt
+        idt[InterruptIndex::Keyboard.as_u8()].set_handler_fn(keyboard_interrupt_handler);
+        idt
     };
 }
 
@@ -43,13 +45,12 @@ extern "x86-interrupt" fn double_fault_handler(
 #[repr(u8)]
 pub enum InterruptIndex {
     Timer = PIC_1_OFFSET,
+    Keyboard,
 }
-
 impl InterruptIndex {
     fn as_u8(self) -> u8 {
         self as u8
     }
-
     fn as_usize(self) -> usize {
         usize::from(self.as_u8())
     }
@@ -63,3 +64,35 @@ extern "x86-interrupt" fn timer_interrupt_handler(
         PICS.lock().notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
     }
 }
+
+
+extern "x86-interrupt" fn keyboard_interrupt_handler(
+    _stack_frame: InterruptStackFrame) {
+    
+    use x86_64::instructions::port::Port;
+    let mut port =Port::new(0x60);
+    let scancode:u8=unsafe{port.read()};
+    let key = match scancode{
+        0x02=>Some('1'),
+        0x03=>Some('2'),
+        0x04=>Some('3'),
+        0x05=>Some('4'),
+        0x06=>Some('5'),
+        0x07=>Some('6'),
+        0x08=>Some('7'),
+        0x09=>Some('8'),
+        0x0a=>Some('9'),
+        0x0b=>Some('0'),
+        _ => None,
+    };
+    if let Some(key)=key{
+        print!("{}",key);
+    }
+
+    unsafe {
+        PICS.lock()
+            .notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
+        }
+}
+
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 //
-
 #![no_std]
 #![feature(abi_x86_interrupt)]
 
